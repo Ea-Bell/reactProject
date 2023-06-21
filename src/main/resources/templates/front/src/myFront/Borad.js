aca9248f755ef58f4bedf71e1b630f01b8b9ae30
Ea-Bell <cjp975@naver.com>
2023-06-21 20:18:13 +0900

    useState 최적화 && js,spring에 cors작업 주석처리

--- src/main/resources/templates/front/src/myFront/Borad.js
+++ src/main/resources/templates/front/src/myFront/Borad.js
@@ -1,9 +1,53 @@
-const Borad= ()=>{
+import {useEffect, useState} from "react";
+
+export default function Borad (){
+
+    const [sendData, setSendData]=useState({
+        text:''
+    }) // json형태로 보내야 서버에서 map으로 받을 수 있다.
+
+    const {text}= sendData;
+
+    useEffect(()=>{
+        fetch("/first",{
+            method: "GET",
+            headers:{
+                "Content-Type": "application/json"
+            }
+        })
+            .then((res)=> { return res.json()})
+            .then(data=>{
+                console.log(data);
+            })
+    }, [])
+    const onclick=()=>{
+        console.log("onclick", sendData)
+
+        fetch("/text",{
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body:JSON.stringify(sendData)
+        })
+            .then((res)=> { return res.json()})
+            .then(data=>{
+                console.log(data);
+            })
+
+        console.log(sendData)
+    }
+
+    const inputText=(e)=>{
+        setSendData({
+            ...sendData,                    //spread 문법 기존에 있는 값들을 가져온다.
+            [e.target.name]:e.target.value  // 해당 배열의 값은 변경해준다.
+        });
+    }
     return(
         <>
-            <button type={"button"} className={"btn btn-primary"}>Default</button>
+            <input name="text" onChange={inputText}/>
+            <button type={"button"} className={"btn btn-primary"} onClick={onclick}>Default</button>
         </>
     )
 }
-
-export default Borad;
